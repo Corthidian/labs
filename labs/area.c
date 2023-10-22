126192f78eef1391ab5ea951eb7624c9d0d293f0
Corthidian <lucifer880799@gmail.com>
2023-10-22 17:05:14 +0500

    did the calculations and printed the result

--- labs/area.c
+++ labs/area.c
@@ -7,5 +7,9 @@ int main (int argc, char *argv[])
         printf("Usage: %s <base> <height>\n",argv [0]);
         return 1;
     }
-    double base = atof(argv{1});
+    double base = atof(argv[1]);
+    double height = atof(argv[2]);
+    double area = 0.5 * base * height;
+    printf("The area of the triangle is: %.2lf\n",area);
+    return 0;
 }
