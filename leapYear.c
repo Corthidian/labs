d733d17f22aaabd988601d3e1e948bcbfc0d0573
Corthidian <lucifer880799@gmail.com>
2023-10-22 21:41:13 +0500

    adding more if statement for more options

--- leapYear.c
+++ leapYear.c
@@ -17,9 +17,92 @@ int main (argc, char **argv)
   printf("test case 1: year = %d:",year);
   if(!isleapyear(year))
   {
+    printf("failed"\n);
+    numfailed = numfailed + 1;
+  }
+  else 
+  {
+    printf("passed \n");
+    numpassed = numpassed +1;
+  }
+  year = 2001;
+  printf("test case 2: year = %d",year);
+  if (isleapyear(year))
+  {
+    printf("failed \n");
+    numfailed = numfailed + 1;
+  }
+  else 
+  {
+    printf("passed\n");
+    numpassed=numpassed+1;
+
+  }
+  year = 2100;
+  printf("test case 3 : year %d",year);
+  if (isleapyear(year))
+  {
+    printf("failed\n");
+    numfailed = numfailed +1;
+  } 
+  else 
+  {
+    printf("passed\n");
+    numpassed=numpassed+1;
+
+  }
+  years = 2004;
+  printf("test case 4 : year %d",year);
+  if (isleapyear(year))
+  {
+    printf("passed\n");
+    numfailed = numfailed +1;
+  } 
+  else 
+  {
+    printf("failed\n");
+    numpassed=numpassed+1;
+
+  }
+  year = 2001;
+  printf("Test Case 5: year = %d: ", year);
+  if
+ (isLeapYear(year)) {
     
+printf
+(
+"FAILED!\n"
+);
+    numFailed = numFailed + 
+1
+;
+  } 
+else
+ {
+    (printf"PASSED!\n");
+    numPassed = numPassed + 1;
+
   }
 
+  year = 2003;
+  
+printf("Test Case 6: year = %d: ", year);
+  
+if
+ (isLeapYear(year)) 
+ {
+    
+printf("FAILED!\n");
+    numFailed = numFailed + 1;
+
+  } 
+else
+ {
+    (printf"PASSED!\n");
+    numPassed = numPassed + 1;
+  }
+
+  
 
 
 }
