60fb04aa8ee50b1f2ade8bb000078654486d8f2a
Corthidian <lucifer880799@gmail.com>
2023-10-22 17:00:16 +0500

    adding strings arrays an if statement and a print statement as well as declaring a double variable for converison

---
+++ labs/area.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main (int argc, char *argv[]) 
+{
+    if (argc != 3) 
+    {
+        printf("Usage: %s <base> <height>\n",argv [0]);
+        return 1;
+    }
+    double base = atof(argv{1});
+}
