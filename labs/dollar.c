22dd9c335935a4e60cd7da15a24242f58379dde6
Corthidian <lucifer880799@gmail.com>
2023-10-22 16:50:01 +0500

    final outout

--- labs/dollar.c
+++ labs/dollar.c
@@ -7,7 +7,6 @@ int main () {
     usd = usd - fee;
     gpb = 0.395 * usd;
     jpy = 62.825 * usd;
-    
-
-
+    printf("Fee (10%%) : $%.2f\nYou get:\n%.2f GBP\n%d JPY\n",fee,gpb,(int)jpy);
+    return 0;
 }
